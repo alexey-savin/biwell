0c5fe1e114bbc301c5d61d933bfee3f0ecd944ea
alexey-savin <alexeyv.savin@gmail.com>
2016-11-30 17:47:31 +0300

    - item weight controller - item weight admin panel

---
+++ BiWell/BiWell.Payment/Controllers/ItemController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BiWell.Payment;
+
+namespace BiWell.Payment.Controllers
+{
+    public class ItemController : Controller
+    {
+        private BiWellEntities db = new BiWellEntities();
+
+        // GET: Items
+        public ActionResult Index()
+        {
+            return View(db.ItemWeights.ToList());
+        }
+
+        // GET: Items/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemWeight itemWeight = db.ItemWeights.Find(id);
+            if (itemWeight == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemWeight);
+        }
+
+        // GET: Items/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Items/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ItemId,Weight")] ItemWeight itemWeight)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ItemWeights.Add(itemWeight);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(itemWeight);
+        }
+
+        // GET: Items/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemWeight itemWeight = db.ItemWeights.Find(id);
+            if (itemWeight == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemWeight);
+        }
+
+        // POST: Items/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ItemId,Weight")] ItemWeight itemWeight)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(itemWeight).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(itemWeight);
+        }
+
+        // GET: Items/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemWeight itemWeight = db.ItemWeights.Find(id);
+            if (itemWeight == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemWeight);
+        }
+
+        // POST: Items/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            ItemWeight itemWeight = db.ItemWeights.Find(id);
+            db.ItemWeights.Remove(itemWeight);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
